e34e7cb24cb65f74e1258a09f6abc2935260e179
Hafied <darka433@gmail.com>
2025-07-22 14:02:23 +0700

    3rd Commit " create hero section, course section and course card "

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import { AppContext } from "./AppContext";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
+
+  const [allCourses, setAllCourses] = useState([]);
+
+  // Fetch All Courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
+    }
+
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+
+    return totalRating / course.courseRatings.length;
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+    navigate,
+    calculateRating,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
